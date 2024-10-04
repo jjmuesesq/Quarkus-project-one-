d68df1d8ad4717a295b9ce5ae9977f972bd0765b
jjmuesesq <jjmuesesq@unal.edu.co>
2024-10-04 17:40:27 -0500

    :sparkles: Definir manualmente el metodo main e incluir logica al inicio de la aplicacion

---
+++ src/main/java/org/digitalthinking/CustomMain.java
@@ -0,0 +1,24 @@
+package org.digitalthinking;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class CustomMain {
+
+    public static void main( String ...args){
+        Quarkus.run(CustomApp.class, args);
+    }
+
+    public static class CustomApp implements QuarkusApplication{
+
+        @Override
+        public int run(String... args) throws Exception {
+            System.out.println("Runing main method from CustomApp");
+            Quarkus.waitForExit();
+            return 0;
+        }
+    }
+
+}
