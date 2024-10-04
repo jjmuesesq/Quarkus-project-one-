d13c9f5c4163dd529bddf61208793ca01b345b38
jjmuesesq <jjmuesesq@unal.edu.co>
2024-10-04 01:48:33 -0500

    :sparkles: Hot reload y como leer propiedades definidas en .properties

--- src/main/java/org/digitalthinking/ExampleResource.java
+++ src/main/java/org/digitalthinking/ExampleResource.java
@@ -2,15 +2,27 @@ package org.digitalthinking;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
 public class ExampleResource {
+
+    @ConfigProperty(name = "greeting")
+    private String greeting;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("custom/{name}")
+    public String customHello(@PathParam("name") String name){
+        return greeting + " " + name + " how are you doing?";
     }
 }
